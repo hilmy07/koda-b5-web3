ec0bae279baca98648ae45fd60fc5ea7dd39e78b
unknown <haidaraly98@gmail.com>
2025-11-07 16:07:36 +0700

    feat: add minitask10

---
+++ minitask10.js
@@ -0,0 +1,21 @@
+
+let mataUang = ["Rupiah", "Dollar", "Yen", "Euro"];
+
+// // for (let i = 0; i < mataUang.length; i++) {
+// //     console.log(mataUang[i]);
+// // }
+
+// // console.log(mataUang.slice(1));
+
+mataUang.push("Pound Sterling");
+
+console.log(mataUang);
+
+// push pake spread operator
+
+mataUang = [...mataUang, "Franc"];
+
+console.log(mataUang);
+
+
+
